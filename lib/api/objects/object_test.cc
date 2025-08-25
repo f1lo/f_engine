ec984ce04aeb9a255473447eb1ca24be776b0ebc
f1lo <nishn.giorgi@gmail.com>
2025-08-25 23:32:42 +0200

    Fix formatting.

--- lib/api/objects/object_test.cc
+++ lib/api/objects/object_test.cc
@@ -20,7 +20,7 @@ class DummyObject : public Object {
 
   void Update(
       const std::list<std::unique_ptr<Object>>& other_objects) override {}
-  void Draw() const override {};
+  void Draw() const override {}
   bool OnCollisionCallback(Object& other_object) override { return true; }
 };
 
